cf424843c3655b90822bc65c19ff57d090c0c26c
Lokeshwer <71865903+Lokesh2002@users.noreply.github.com>
2023-08-30 18:52:39 +0530

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -3,12 +3,10 @@ import { Col, Row } from "react-bootstrap";
 import {
   DiJavascript1,
   DiReact,
-  DiNodejs,
   DiPython,
   DiHtml5,
   DiCss3,
   DiGit,
-  DiJava,
 } from "react-icons/di";
 
 function Techstack() {
@@ -25,9 +23,7 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
+      
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
       </Col>
@@ -37,9 +33,7 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJava />
-      </Col>
+     
     </Row>
   );
 }
