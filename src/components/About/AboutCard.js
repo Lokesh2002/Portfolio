a447c7fa6d78c4cffc11d05396ae5519459dbab8
Lokeshwer <71865903+Lokesh2002@users.noreply.github.com>
2025-01-23 22:53:46 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -10,16 +10,20 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Lokeshwer  </span>
             from <span className="purple"> Chennai, India.</span>
-            <br />I am an entry-level software engineer who has just completed a B.E. in Computer Science Engineering.
+            <br />I am an entry-level software engineer with a B.E. in Computer Science & Engineering.
             <br />
-            Currently I am working as an intern in scripting engineer at fiserv and simultaneously learning and developing user interactive websites & web applications and also to be a software engineer.
+             I recently completed an internship as a Scripting Engineer at Fiserv, where I gained valuable hands-on experience in scripting and web development.
+
+Alongside my internship, I have been focusing on learning and developing interactive products, honing my skills in creating innovative and user-centric solutions. My goal is to continue improving my technical expertise and evolve as a software engineer, with a strong emphasis on building scalable and efficient products.
+
+I’m always eager to explore new technologies and challenges that will help me grow as a developer and contribute meaningfully to any project I work on.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Watching Movies and Documentaries
             </li>
             <li className="about-activity">
               <ImPointRight /> Writing Stories for short films
