f12652f00fa01a4f4257a25c1462b2661c8a20c0
Lokeshwer <71865903+Lokesh2002@users.noreply.github.com>
2025-01-23 22:57:20 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import pic from "../../Assets/pic.jpg";
+import pic from "../../Assets/pic.JPG";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
